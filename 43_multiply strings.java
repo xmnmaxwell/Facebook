bccaca9c732e51adb93e657e211ae93dd279a6a0
xmn880819 <xmn880819@tamu.edu>
2018-12-01 22:14:27 -0600

    Create 43_multiply strings.java

---
+++ 43_multiply strings.java
@@ -0,0 +1,31 @@
+43. multiply strings 
+O(mn)
+public String multiply(String num1, String num2) {
+        if (num1 == null || num2 == null) return null; 
+        int len1 = num1.length();
+        int len2 = num2.length();
+        int len3 = len1 + len2;
+        int product = 0;
+        int[] num3 = new int[len3];
+        int i, j;
+        for(i = len1 - 1; i >= 0; i--){
+            int carry = 0;
+            for (j = len2 - 1; j >= 0; j--){
+                
+                product = carry + num3[i + j + 1] + (num1.charAt(i)- '0')*(num2.charAt(j) - '0');
+                num3[i + j+ 1] = product % 10;//把余数存储在num3数组里；
+                carry = product / 10;
+            }
+            num3[i + j + 1] = carry;// 为下一次循环引入进位数；
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        i = 0;
+        while (i < len3 - 1 && num3[i] == 0){
+            i++; // 过掉num3前面的无用的0
+        }
+        while(i < len3){
+            sb.append(num3[i++]);
+        }
+        return sb.toString();
+    }
