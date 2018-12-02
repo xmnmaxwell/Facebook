e8f2f347a88951dbc82e7b6c691246d5cd05206e
xmn880819 <xmn880819@tamu.edu>
2018-12-01 22:02:42 -0600

    Create 703_Kth Largest Element in a Stream.java

---
+++ 703_Kth Largest Element in a Stream.java
@@ -0,0 +1,34 @@
+703. Kth Largest Element in a Stream
+O(nlogk)
+在一个流动的数据中找到返回第k大的数 定义一个k size的pq,如果pq没满继续加，满了就比较头
+和新加入的谁大，val大就替换掉pq 的头
+class KthLargest {
+    final PriorityQueue<Integer> q;
+    // define pq to store the data streaming with orders
+    final int k;
+    
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        q = new PriorityQueue<>(k);// define a new pq with the size k.
+        for (int n : nums){
+             if(q.size() < k){
+                q.add(n);
+            }else if (q.peek() < n){
+                q.poll();
+                q.offer(n);
+            } //this add function add n with ascending orders
+        }
+        
+    }
+    
+    public int add(int val) {
+        if (q.size() < k){
+            q.offer(val); // if the q size is less than k, we add that val in q 
+        } else if (q.peek() < val){
+        //q.peek is the kth largest number, if q.peek<val, replace with val
+            q.poll();
+            q.offer(val);
+        }
+        return q.peek();
+    }
+}
