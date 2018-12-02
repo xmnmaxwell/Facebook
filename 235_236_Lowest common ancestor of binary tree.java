34c115699312e4780d9c173f79412c0027b83703
xmn880819 <xmn880819@tamu.edu>
2018-12-01 19:21:00 -0600

    Update and rename 236_Lowest common ancestor of binary tree.java to 235_236_Lowest common ancestor of binary tree.java

--- 235_236_Lowest common ancestor of binary tree.java
+++ 235_236_Lowest common ancestor of binary tree.java
@@ -15,3 +15,14 @@ public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
         return null;
     }
+***************************************************************************************************
+235. Lowest Common Ancestor of a Binary Search Tree BST找到两个指定节点的最底层的共同祖先
+// worst case O(n) time O(n) stack
+public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) 
+            return lowestCommonAncestor(root.left, p, q);
+        else if (root.val < p.val && root.val < q.val)
+            return lowestCommonAncestor(root.right, p, q);
+        else return root;
+        
+    }
