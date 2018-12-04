348e284b1d84a293743720500ad467576863edef
xmn880819 <xmn880819@tamu.edu>
2018-12-04 14:33:18 -0600

    Update 680_valid palindrome II.java

--- 680_valid palindrome II.java
+++ 680_valid palindrome II.java
@@ -27,3 +27,42 @@ public boolean validPalindrome(String s) {
         }
         return true;
      }
+**********************************************************************************************
+在一个字符串里找到一个某一个字母使得删掉它就变成回文串了
+int possiblePalinByRemovingOneChar(string str) 
+{ 
+    //  Initialize low and right by both the ends of the string 
+    int low = 0, high = str.length() - 1; 
+  
+    //  loop untill low and high cross each other 
+    while (low < high) 
+    { 
+        // If both characters are equal then move both pointer 
+        // towards end 
+        if (str[low] == str[high]) 
+        { 
+            low++; 
+            high--; 
+        } 
+        else
+        { 
+            /*  If removing str[low] makes the whole string palindrome. 
+                We basically check if substring str[low+1..high] is 
+                palindrome or not. */
+            if (isPalindrome(str.begin() + low + 1, str.begin() + high)) 
+                return low; 
+  
+            /*  If removing str[high] makes the whole string palindrome 
+                We basically check if substring str[low+1..high] is 
+                palindrome or not. */
+            if (isPalindrome(str.begin() + low, str.begin() + high - 1)) 
+                return high; 
+  
+            return -1; 
+        } 
+    } 
+  
+    //  We reach here when complete string will be palindrome 
+    //  if complete string is palindrome then return mid character 
+    return -2; 
+} 
