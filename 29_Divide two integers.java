80fbadba476ada00f9da181a0a6d24c5b0cd4578
xmn880819 <xmn880819@tamu.edu>
2018-12-04 14:46:26 -0600

    Update 29_Divide two integers.java

--- 29_Divide two integers.java
+++ 29_Divide two integers.java
@@ -7,11 +7,9 @@ O(log(dividend))
 这道题主要考察数据溢出的问题，首先一种情况是除数有可能是min，被除数-1，这得到是max+1 数据溢出
 排除这种情况，还有就是除数为0
 
-将除数被除数变成long型，初始化shift为0，每向左移一位加一倍,这样可以不用一个个累加把复杂度提升到了logn
-a = 25, b = 3, shift = 1, res = 1
-    22      6          2       1+2
-    16      12         4       1+2+4
-     4      
+将除数被除数变成long型，初始化shift为0，将shift调到最大使得  a>= b*2^shift and a < b*2^(shift+1)
+a = 28, b = 3, shift = 1, res = 1
+    
 
  public int divide(int dividend, int divisor) {
        if (dividend == 0) {
@@ -22,15 +20,15 @@ a = 25, b = 3, shift = 1, res = 1
         }
         boolean isNegative = ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor>0));
         long a = Math.abs((long)dividend); // a 是min，但按这个算法，a就变成max了，要用long型
-        long b = Math.abs((long) divisor);
+        long b = Math.abs((long) divisor); // b总要向前移一位有可能会数据溢出所以要用long型
         int res = 0;
-        while (a >= b){
+        while (a >= b){ // a = 25
             int shift = 0;
             while (a >= (b << shift)){ //if the remaining of a is still larger than b*2^shift
-                shift++;
+                shift++; 28>3, shift: 1/ 28>6, shift:2 / 28>12, shift:3 / 28>24, shift:4
             }
-            a -= b << (shift-1);// a要减去没移前的位数
-            res += 1 << (shift - 1);       
+            a -= b << (shift-1);// a要减去没移前的位数 // b<<(4-1) = 24
+            res += 1 << (shift - 1); // res = 8,
         }
      return res = isNegative ? -res : res;
     }
