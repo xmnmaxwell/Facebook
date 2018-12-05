8b849a6f3486868ca2dd4e8258ae219fb1005d8b
xmn880819 <xmn880819@tamu.edu>
2018-12-05 11:02:10 -0600

    Create 139_word break.java

---
+++ 139_word break.java
@@ -0,0 +1,32 @@
+139. Word Break
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s c
+an be segmented into a space-separated sequence of one or more dictionary words.
+
+Note:
+
+The same word in the dictionary may be reused multiple times in the segmentation.
+You may assume the dictionary does not contain duplicate words.
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet", "code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+Time complexity : O(n^2)
+Space complexity : O(n). Length of pp array is n+1.
+
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet=new HashSet(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
