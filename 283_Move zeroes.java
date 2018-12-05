25afa1782c72153cba88048ec5a027906948af51
xmn880819 <xmn880819@tamu.edu>
2018-12-05 16:32:02 -0600

    Update 283_Move zeroes.java

--- 283_Move zeroes.java
+++ 283_Move zeroes.java
@@ -15,15 +15,16 @@ class Solution {
             nums[now ++] = 0;//if now is less, assign the rest element 0.
     }
 }
-
+// 1 2 3 0 2 0 //1 2 3 2 0
+读取最少
 class Solution {
     public void moveZeroes(int[] nums) {
         int j = 0;
         for (int i = 0; i < nums.length; i ++)
-            if (nums[i] != 0) {
+            if (nums[i] != 0) {//一开始没有0的时候，i j同步，位置不变，但凡有0了，i就会加1，j不变，
                 if (i == j) j ++;
                 else {
-                    nums[j ++] = nums[i];
+                    nums[j ++] = nums[i];//这个时候让落后的j = num[i], 然后把0放在number[i]里
                     nums[i] = 0;
                 }
             }
