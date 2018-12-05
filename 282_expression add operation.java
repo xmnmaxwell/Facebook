c046b2461fdc048b028bb9d2a81921f4fc0187f6
xmn880819 <xmn880819@tamu.edu>
2018-12-04 19:30:55 -0600

    Update 282_expression add operation.java

--- 282_expression add operation.java
+++ 282_expression add operation.java
@@ -5,30 +5,59 @@ overflow: we use a long type once it is larger than Integer.MAX_VALUE or minimum
 0 sequence: because we can't have numbers with multiple digits started with zero, we have to deal with it too.
 a little trick is that we should save the value that is to be multiplied in the next recursion.
 
-  public List<String> addOperators(String num, int target) {
-        List<String> rst = new ArrayList<String>();
-        if(num == null || num.length() == 0) return rst;
-        helper(rst, "", num, target, 0, 0, 0);
-        return rst;
-    }
-    public void helper(List<String> rst, String path, String num, int target, int pos, long eval, long multed){
-        if(pos == num.length()){
-            if(target == eval)
-                rst.add(path);
+ void dfs(String num, int target, int start, String str, long sum, long lastF, List<String> ans) {
+        if (start == num.length()) {
+            if (sum == target) {
+                ans.add(str);
+            }
             return;
         }
-        for(int i = pos; i < num.length(); i++){
-            if(i != pos && num.charAt(pos) == '0') break;
-            long cur = Long.parseLong(num.substring(pos, i + 1));
-            if(pos == 0){
-                helper(rst, path + cur, num, target, i + 1, cur, cur);
+        for (int i = start; i < num.length(); i++) {
+            long x = Long.parseLong(num.substring(start, i + 1));
+
+            if (start == 0) {
+                dfs(num, target, i + 1, "" + x, x, x, ans);
+            } else {
+                dfs(num, target, i + 1, str + "*" + x, sum - lastF + lastF * x, lastF * x, ans);
+                dfs(num, target, i + 1, str + "+" + x, sum + x, x, ans);
+                dfs(num, target, i + 1, str + "-" + x, sum - x, -x, ans);
             }
-            else{
-                helper(rst, path + "+" + cur, num, target, i + 1, eval + cur , cur);
-                
-                helper(rst, path + "-" + cur, num, target, i + 1, eval -cur, -cur);
-                
-                helper(rst, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur );
+            if (x == 0) {
+                break;
             }
+        }
+    }
+
+    public List<String> addOperators(String num, int target) {
+        // Write your code here
+        List<String> ans = new ArrayList<>();
+        dfs(num, target, 0, "", 0, 0, ans);
+        return ans;
+    }
+    ******************************************************************************************
+    简单版只有+-
+      public List<String> addOperators(String num, int target) {
+        List<String> ans = new ArrayList<String>();
+        search(0, 0, target, "", num, ans);
+        return ans;
+    }
+    
+    private void search(int index, long sum, int target, String prefix, String s, List<String> ans) {
+        if (index == s.length()) {
+            if (sum == target)
+                ans.add(prefix);
+            return;
+        }
+        
+        long num = 0;
+        for (int i = index; i < s.length(); i ++) {
+            num = num * 10 + s.charAt(i) - '0';
+            if (index == 0)
+                search(i + 1, num, target, "" + num, s, ans);
+            else {
+                search(i + 1, sum + num, target, prefix + "+" + num, s, ans);
+                search(i + 1, sum - num, target, prefix + "-" + num, s, ans);
+            }
+            if (num == 0) break;
         }
     }
