e8f214c1bd0a10e4160dc01f041c9dfb33c19f5b
xmn880819 <xmn880819@tamu.edu>
2018-12-05 22:42:39 -0600

    Update 10_regular expression match.java

--- 10_regular expression match.java
+++ 10_regular expression match.java
@@ -12,11 +12,10 @@ Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore, by
 DFS:
 class Solution {
     public boolean isMatch(String s, String p) {
-        if (p == null || p.length() == 0)
+        if (p == null || p.length() == 0) //p是空的或者长度为0，看s是不是也是
             return (s == null || s.length() == 0);
-        if (p.length() == 1 || p.charAt(1) != '*')
-            return (s.length() > 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.') &&
-                isMatch(s.substring(1), p.substring(1)));
+        if (p.length() == 1 || p.charAt(1) != '*')//如果p长度1，或者p第二个字母不是* a/*a
+            return (s.length() > 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.') &&  isMatch(s.substring(1), p.substring(1)));
         return (isMatch(s, p.substring(2)) || 
                 (s.length() > 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.') && isMatch(s.substring(1), p)));
     }
