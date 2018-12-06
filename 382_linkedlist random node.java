865e188a53813129147902e13ee53b0bd65897d1
xmn880819 <xmn880819@tamu.edu>
2018-12-06 08:44:09 -0600

    Update 382_linkedlist random node.java

--- 382_linkedlist random node.java
+++ 382_linkedlist random node.java
@@ -1,6 +1,8 @@
 382. linkedlist random node
-public class Solution {
-    
+Given a singly linked list, return a random node's value from the linked list.
+Each node must have the same probability of being chosen.
+
+public class Solution {    
     ListNode head;
     Random random;
     
@@ -10,15 +12,12 @@ public class Solution {
     }
     
     public int getRandom() {
-        
-        ListNode c = head;
-        int r = c.val;
-        for(int i=1;c.next != null;i++){
-            
-            c = c.next;
-            if(random.nextInt(i + 1) == i) r = c.val;                        
+        ListNode node = head;
+        int r = node.val;
+        for(int i = 1; node.next != null; i++){
+            node = node.next;
+            if(random.nextInt(i + 1) == i) r = node.val; // 1: 1*1/2*2/3, 2: 1/2*2/3, 3:1/3              
         }
-        
         return r;
     }
 }
