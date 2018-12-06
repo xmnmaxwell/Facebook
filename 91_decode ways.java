9aa78b3872f01406e8c66067dece00077f2b45e1
xmn880819 <xmn880819@tamu.edu>
2018-12-05 20:03:23 -0600

    Update 91_decode ways.java

--- 91_decode ways.java
+++ 91_decode ways.java
@@ -26,3 +26,23 @@ public int numDecodings(String s) {
         }    
         return nums[s.length()];
     }
+******************************************************************************************************************
+You are climbing a stair case. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+public int climbStairs(int n) {
+    if(n < 0)
+        return 0;
+    if(n == 1)
+        return 1;
+        
+    int[] store = new int[n];
+    
+    store[0] = 1;
+    store[1] = 2;
+    
+    for(int i = 2; i < n; ++i)
+        store[i] = store[i-1] + store[i-2];
+        
+    return store[n-1];
+}
