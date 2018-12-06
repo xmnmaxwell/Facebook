bdd1c595a09cdbba39530455149dcf9abd60a70d
xmn880819 <xmn880819@tamu.edu>
2018-12-05 20:04:58 -0600

    Update 121_122_123_Best time to buy and sell stock.java

--- 121_122_123_Best time to buy and sell stock.java
+++ 121_122_123_Best time to buy and sell stock.java
@@ -27,10 +27,14 @@ public int maxProfit(int[] prices) {
         return max;
     }
 f123. Best time to buy and sell stock III,只可以买卖两次
-First assume that we have no money, so buy1 means that we have to borrow money from others, we want to borrow less so that we have to make our balance as max as we can(because this is negative).
-sell1 means we decide to sell the stock, after selling it we have price[i] money and we have to give back the money we owed, so we have price[i] - |buy1| = prices[i ] + buy1, we want to make this max.
-buy2 means we want to buy another stock, we already have sell1 money, so after buying stock2 we have buy2 = sell1 - price[i] money left, we want more money left, so we make it max
-sell2 means we want to sell stock2, we can have price[i] money after selling it, and we have buy2 money left before, so sell2 = buy2 + prices[i], we make this max.
+First assume that we have no money, so buy1 means that we have to borrow money from others, we want to borrow less so that 
+we have to make our balance as max as we can(because this is negative).
+sell1 means we decide to sell the stock, after selling it we have price[i] money and we have to give back the money we owed, 
+so we have price[i] - |buy1| = prices[i ] + buy1, we want to make this max.
+buy2 means we want to buy another stock, we already have sell1 money, so after buying stock2 we have buy2 = sell1 - price[i] 
+money left, we want more money left, so we make it max
+sell2 means we want to sell stock2, we can have price[i] money after selling it, and we have buy2 money left before, 
+so sell2 = buy2 + prices[i], we make this max.
 So sell2 is the most money we can have.
 
 public int maxProfit(int[] prices) {
