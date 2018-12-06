16b67453c409c4ab711fab9d87e103b2fad9af95
xmn880819 <xmn880819@tamu.edu>
2018-12-05 18:26:52 -0600

    Update 133_clone graph.java

--- 133_clone graph.java
+++ 133_clone graph.java
@@ -1,5 +1,5 @@
 DFS//
-O(V+E)
+O(V+E) O(V+E)
 public class Solution {
     Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();//key是老图，value是新图
     
@@ -10,7 +10,7 @@ public class Solution {
         map.put(node, new UndirectedGraphNode(node.label));//if it does not contain node.将label放在新图里
         List<UndirectedGraphNode> list = node.neighbors;// 建一个新的list存放图的neighbour
         for (UndirectedGraphNode nb : list)//遍历neighbour
-            map.get(node).neighbors.add(cloneGraph(nb));//将neighbour放进value里
+            map.get(node).neighbors.add(cloneGraph(nb));//map.get(node).neighbors是指新图的neigbour指针
         return map.get(node);
     }
 }
