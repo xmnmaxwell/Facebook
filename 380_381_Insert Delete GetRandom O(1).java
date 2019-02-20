d41b43bd196cbdefb12e6518433a5ca8cbcef853
xmn880819 <xmn880819@tamu.edu>
2019-02-20 06:00:31 -0800

    Update 380_381_Insert Delete GetRandom O(1).java

--- 380_381_Insert Delete GetRandom O(1).java
+++ 380_381_Insert Delete GetRandom O(1).java
@@ -17,27 +17,23 @@ public class RandomizedSet {
         nums = new ArrayList<Integer>();
         num2index = new HashMap<Integer, Integer>();  
         rand = new Random();
-    }
-    
+    }    
     // Inserts a value to the set
     // Returns true if the set did not already contain the specified element or false
     public boolean insert(int val) {
         if (num2index.containsKey(val)) {
             return false;
-        }
-        
+        }     
         num2index.put(val, nums.size());
         nums.add(val);
         return true;
     }
-    
     // Removes a value from the set
     // Return true if the set contained the specified element or false
     public boolean remove(int val) {
         if (!num2index.containsKey(val)) {
             return false; 
         } 
-        
         int index = num2index.get(val);
         if (index < nums.size() - 1) { // not the last one than swap the last one with this val
             int last = nums.get(nums.size() - 1);
@@ -48,7 +44,6 @@ public class RandomizedSet {
         nums.remove(nums.size() - 1);
         return true;
     }
-    
     // Get a random element from the set
     public int getRandom() {
         return nums.get(rand.nextInt(nums.size()));
