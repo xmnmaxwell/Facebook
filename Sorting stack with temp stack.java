4203e2aaa3e6ec3b57476dcd4243533836f50b50
xmn880819 <xmn880819@tamu.edu>
2019-02-21 14:11:00 -0600

    Create Sorting stack with temp stack.java

---
+++ Sorting stack with temp stack.java
@@ -0,0 +1,24 @@
+O(n2)
+*********************************************************
+class SortStack { 
+    // This function return the sorted stack 
+    public Stack<Integer> sortstack(Stack<Integer> input) { 
+        Stack<Integer> tmpStack = new Stack<Integer>(); 
+        while(!input.isEmpty()) { 
+            // pop out the first element 
+            int tmp = input.pop(); 
+          
+            // while temporary stack is not empty and 
+            // top of stack is greater than temp 
+            while(!tmpStack.isEmpty() && tmpStack.peek() > tmp){ 
+                // pop from temporary stack and  
+                // push it to the input stack 
+                  input.push(tmpStack.pop()); 
+            } 
+              
+            // push temp in tempory of stack 
+            tmpStack.push(tmp); 
+        } 
+        return tmpStack; 
+    }
+}    
